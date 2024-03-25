3e07df6d3e5134f17a1241dd8f2c9915842b5a5a
47289 <759100268@qq.com>
2024-03-25 22:25:23 +0800

    test

---
+++ redis-demo/redis5/src/test/java/com/example/redis5/Redis5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redis5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Redis5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
